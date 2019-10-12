723487b6ce66e593da5ebc084e3b8c76272874e0
Marsilea149 <bingbingwu213@gmail.com>
2019-10-12 16:59:19 -0700

    Initial Commit

---
+++ module2.py
@@ -0,0 +1,17 @@
+import math
+import sys
+from os import rename
+
+import requests
+
+print(sys.version)  # show the version of python
+print(sys.executable)  # show the location of python executable
+
+
+def greet(who_to_greet):
+    greeting = "hello, {}".format(who_to_greet)
+    return greeting
+
+
+print(greet("World"))
+print(greet("Bingbing"))
